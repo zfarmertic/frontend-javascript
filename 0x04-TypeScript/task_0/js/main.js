79e3d171fba76987c419619689d152caeca7fca8
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 14:08:07 +0100

    table created succesfully

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -11,43 +11,45 @@ var studentTwo = {
     location: "Delat state, Nigeria"
 };
 var studentList = [studentOne, studentTwo];
-
-
-function createTable(){
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    const tbody = document.createElement("tbody");
-    const headerRow = document.createElement("tr");
-
-    const headerContent = ['First Name', 'Location'];
-
-    headerContent.forEach(text => {
-        const headerH = document.createElement("th");
+// var studentOne = {
+//     firstName: "Erovwo",
+//     lastName: "Igbunuoghene",
+//     age: 45,
+//     location: "Enugu state, Nigeria"
+// };
+// var studentTwo = {
+//     firstName: "Merit",
+//     lastName: "Onogbotsere",
+//     age: 56,
+//     location: "Delat state, Nigeria"
+// };
+// var studentList = [studentOne, studentTwo];
+function createTable() {
+    var table = document.createElement("table");
+    var thead = document.createElement("thead");
+    var tbody = document.createElement("tbody");
+    var headerRow = document.createElement("tr");
+    var headerContent = ['First Name', 'Location'];
+    headerContent.forEach(function (text) {
+        var headerH = document.createElement("th");
         headerH.textContent = text;
-        console.log(headerH)
+        console.log(headerH);
         headerRow.appendChild(headerH);
-        })
-
-        thead.appendChild(headerRow)
-        table.appendChild(thead)
-studentList.forEach(tableBody => {
-    const row = document.createElement("tr")
-    const firstCell = document.createElement("td")
-    firstCell.textContent = tableBody.firstName
-
-    row.appendChild(firstCell)
-    const secondCell = document.createElement("td")
-    secondCell.textContent = tableBody.location
-    row.appendChild(secondCell)
-
-    tbody.appendChild(row)
-})
-  table.appendChild(tbody)
-        
-        const tab = document.getElementById("tab")
-        tab.appendChild(table)
-
-        
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+    studentList.forEach(function (tableBody) {
+        var row = document.createElement("tr");
+        var firstCell = document.createElement("td");
+        firstCell.textContent = tableBody.firstName;
+        row.appendChild(firstCell);
+        var secondCell = document.createElement("td");
+        secondCell.textContent = tableBody.location;
+        row.appendChild(secondCell);
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+    var tab = document.getElementById("tab");
+    tab.appendChild(table);
 }
-
-createTable()
+createTable();
--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,4 +21,57 @@ const studentTwo: Student = {
 
 const studentList = [studentOne, studentTwo]
 
-studentList.map(items => <tr>items.firstName</>)
+
+// var studentOne = {
+//     firstName: "Erovwo",
+//     lastName: "Igbunuoghene",
+//     age: 45,
+//     location: "Enugu state, Nigeria"
+// };
+// var studentTwo = {
+//     firstName: "Merit",
+//     lastName: "Onogbotsere",
+//     age: 56,
+//     location: "Delat state, Nigeria"
+// };
+// var studentList = [studentOne, studentTwo];
+
+
+function createTable(){
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+    const headerRow = document.createElement("tr");
+
+    const headerContent = ['First Name', 'Location'];
+
+    headerContent.forEach(text => {
+        const headerH = document.createElement("th");
+        headerH.textContent = text;
+        console.log(headerH)
+        headerRow.appendChild(headerH);
+        })
+
+        thead.appendChild(headerRow)
+        table.appendChild(thead)
+studentList.forEach(tableBody => {
+    const row = document.createElement("tr")
+    const firstCell = document.createElement("td")
+    firstCell.textContent = tableBody.firstName
+
+    row.appendChild(firstCell)
+    const secondCell = document.createElement("td")
+    secondCell.textContent = tableBody.location
+    row.appendChild(secondCell)
+
+    tbody.appendChild(row)
+})
+  table.appendChild(tbody)
+        
+        const tab = document.getElementById("tab")
+        tab.appendChild(table)
+
+        
+}
+
+createTable()
